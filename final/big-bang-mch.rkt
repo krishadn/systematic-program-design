103934f5f0198b0c3c785da43e960fb1c08cf704
krishadn <krizziasantillan@gmail.com>
2025-09-29 14:26:13 +0800

    prograns from 3a: How to Design Worlds

---
+++ final/big-bang-mch.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname big-bang-mch) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
+;; big-bang mechanism
+;; 1. initial world state (can be any data type that can represent the world state)
+;; 2. function that changes the world state based on the current world state
+;; 3. function that shows the current world state
+
+
