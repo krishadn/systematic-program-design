27615f87f2e5d14d10c57c11bda239f806bd786c
krishadn <krizziasantillan@gmail.com>
2025-10-26 19:02:12 +0800

    program from taking 6a Binary Search Trees

---
+++ final/list-abbrev.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname list-abbrev) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;(define LIST1 (list "a" "b" "c"))
+
+;(first LIST1)
+;(rest LIST1)
+
+;(cons (cons 1 empty) (cons (cons 2 empty) empty))
+
+(define L1 (list 1 2 3))
+(define L2 (list 4 5 6))
+
+; (cons 0 L1)
+
+; (list 0 L1)
+
+(append L1 L2)
+(list)
+empty
